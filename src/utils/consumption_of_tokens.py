b52c1ace811c8ec0885d30dd3394e736438a9911
perwaaler <perwaaler@gmail.com>
2024-01-24 14:20:48 +0100

    created module consumption_of_tokens.py which contains function relating to chat consumption of resources.

---
+++ src/utils/consumption_of_tokens.py
@@ -0,0 +1,43 @@
+"""Functions (non-essential) used to calculate various metrics related to use of tokens. """
+import tiktoken
+
+from utils.backend import SETTINGS
+from utils.backend import MODEL_ID
+
+
+def count_tokens_used_to_create_last_response(conversation) -> int:
+    """Counts the number of tokens used to generate the last message in the conversation."""
+    tokens_in = count_tokens(conversation[:-1])
+    tokens_out = count_tokens(conversation[-1])
+    return tokens_in + tokens_out
+
+
+def calculate_cost_of_response(conversation):
+    """Calculates the cost of generating the last repsonse (which is assumed to be from
+    assistant)."""
+    tokens_in = count_tokens(conversation[:-1])
+    tokens_out = count_tokens(conversation[-1])
+    dollars_tokens_in = tokens_in * SETTINGS["dollars_per_1k_input_token"] / 1000
+    dollars_tokens_out = tokens_out * SETTINGS["dollars_per_1k_output_token"] / 1000
+    kroners_total = (dollars_tokens_in + dollars_tokens_out) * SETTINGS[
+        "kr_to_dollar_ratio"
+    ]
+    return kroners_total
+
+
+def count_tokens(conversation) -> int:
+    """Counts the number of tokens in a conversation. Uses token encoder
+    https://github.com/openai/tiktoken/blob/main/tiktoken/model.py. Input argument can be either a
+    list or a dictionary (for instance the last response in the conversation).
+    """
+    if isinstance(conversation, dict):
+        # Ensure list format
+        conversation = [conversation]
+    encoding = tiktoken.encoding_for_model(MODEL_ID)
+    num_tokens = 0
+    for message in conversation:
+        # "user: assistant:" corresponds to 4 tokens
+        num_tokens += 4
+        num_tokens += len(encoding.encode(message["content"]))
+    num_tokens += 2  # every reply is primed with <im_start>assistant
+    return num_tokens
--- src/utils/general.py
+++ src/utils/general.py
@@ -1,13 +1,8 @@
 """General utilities."""
 
-import tiktoken
 import re
-import logging
 
-from utils.backend import MODEL_ID
 from utils.backend import dump_conversation
-from utils.backend import SETTINGS
-from utils.backend import LOGGER
 from utils.chat_utilities import identify_assistant_responses
 
 
@@ -46,44 +41,6 @@ def rewind_chat_by_n_assistant_responses(n_rewind: int, conversation: list) -> l
     n_rewind = min([n_rewind, len(assistant_indices) - 1])
     index_reset = assistant_indices[-(n_rewind + 1)]
     return conversation[: index_reset + 1]
-
-
-def count_tokens_used_to_create_last_response(conversation) -> int:
-    """Counts the number of tokens used to generate the last message in the conversation."""
-    tokens_in = count_tokens(conversation[:-1])
-    tokens_out = count_tokens(conversation[-1])
-    return tokens_in + tokens_out
-
-
-def calculate_cost_of_response(conversation):
-    """Calculates the cost of generating the last repsonse (which is assumed to be from
-    assistant)."""
-    tokens_in = count_tokens(conversation[:-1])
-    tokens_out = count_tokens(conversation[-1])
-    dollars_tokens_in = tokens_in * SETTINGS["dollars_per_1k_input_token"] / 1000
-    dollars_tokens_out = tokens_out * SETTINGS["dollars_per_1k_output_token"] / 1000
-    kroners_total = (dollars_tokens_in + dollars_tokens_out) * SETTINGS[
-        "kr_to_dollar_ratio"
-    ]
-    return kroners_total
-
-
-def count_tokens(conversation) -> int:
-    """Counts the number of tokens in a conversation. Uses token encoder
-    https://github.com/openai/tiktoken/blob/main/tiktoken/model.py. Input argument can be either a
-    list or a dictionary (for instance the last response in the conversation).
-    """
-    if isinstance(conversation, dict):
-        # Ensure list format
-        conversation = [conversation]
-    encoding = tiktoken.encoding_for_model(MODEL_ID)
-    num_tokens = 0
-    for message in conversation:
-        # "user: assistant:" corresponds to 4 tokens
-        num_tokens += 4
-        num_tokens += len(encoding.encode(message["content"]))
-    num_tokens += 2  # every reply is primed with <im_start>assistant
-    return num_tokens
 
 
 # ## Dealing with linebreak characters ##
