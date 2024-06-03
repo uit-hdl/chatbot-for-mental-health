e77081d628485dd6f0a492793dfa72ca2e5917fd
perwaaler <perwaaler@gmail.com>
2024-06-03 13:20:28 +0200

    Created app.py file

---
+++ app.py
@@ -0,0 +1,13 @@
+"""Script for doing E-health course in gradio chatbot interface."""
+
+import os
+import sys
+
+src_directory = os.getcwd() + "/src"
+sys.path.append(src_directory)
+
+from src.gradio_chat_with_history_and_images import (
+    chat_with_bot_in_gradio_interface,
+)
+
+chat_with_bot_in_gradio_interface(chatbot_id="ehealth_module1")
