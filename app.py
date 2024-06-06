dd5dae7471d7d185a4e44e04a39b4655b440f42d
perwaaler <perwaaler@gmail.com>
2024-06-06 08:56:05 +0200

    Added __main__ block for local development and testing

--- app.py
+++ app.py
@@ -127,3 +127,9 @@ with gr.Blocks() as demo:
 
 
 app = gr.mount_gradio_app(app, demo, path="/")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
