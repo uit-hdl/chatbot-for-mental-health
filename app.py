56cf5845ed522a1b950a30bb7fcb0f007d8e22fd
perwaaler <perwaaler@gmail.com>
2024-06-18 12:56:28 +0200

    renamed gradio_chat_with_history_and_images to gradio_chat.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import sys
 src_directory = os.getcwd() + "/src"
 sys.path.append(src_directory)
 
-from src.gradio_chat_with_history_and_images import (
+from src.gradio_chat import (
     chat_with_bot_in_gradio_interface,
 )
 
